3cdebb5cdae2b76277a0b65b75ad15bf2f24868d
PavelBakhtin <pavelbakhtin88@gmail.com>
2022-10-10 22:12:20 +0300

    5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components';
+
 import './index.css'
 
 
